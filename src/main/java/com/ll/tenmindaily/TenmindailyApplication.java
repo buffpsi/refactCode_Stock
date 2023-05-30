f10af9b12f5d7500b06f1acdcbebe31895bc1b21
nagiltae <nagt1997@gmail.com>
2023-05-30 14:30:36 +0900

    init

---
+++ src/main/java/com/ll/tenmindaily/TenmindailyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.tenmindaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenmindailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenmindailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/tenmindaily/TenmindailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.tenmindaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenmindailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
