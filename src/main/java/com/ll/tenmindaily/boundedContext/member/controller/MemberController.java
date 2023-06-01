6462bb0d62b683e723aa5710ed7cf998cf5441cf
havegrit <79460763+havegrit@users.noreply.github.com>
2023-06-01 14:23:22 +0900

    design: 로그인 페이지 UI 작업 (#7)
    #7 Close

--- src/main/java/com/ll/tenmindaily/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/tenmindaily/boundedContext/member/controller/MemberController.java
@@ -1,4 +1,17 @@
 package com.ll.tenmindaily.boundedContext.member.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usr/member")
 public class MemberController {
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/login")
+    public String showLogin() {
+        return "usr/member/login";
+    }
 }
+
