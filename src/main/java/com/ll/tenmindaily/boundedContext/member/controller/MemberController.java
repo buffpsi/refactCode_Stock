82eb0181c6175bba50cc096fa5e79c24c9c36478
havegrit <shin_jaean@naver.com>
2023-06-08 21:45:15 +0900

    feat: 마이페이지 구현

--- src/main/java/com/ll/tenmindaily/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/tenmindaily/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.tenmindaily.boundedContext.member.controller;
 
+import com.ll.tenmindaily.base.rq.Rq;
 import com.ll.tenmindaily.base.rsData.RsData;
 import com.ll.tenmindaily.boundedContext.member.entity.Member;
 import com.ll.tenmindaily.boundedContext.member.service.MemberService;
@@ -11,15 +12,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    private final Rq rq;
 
     @AllArgsConstructor
     @Getter
@@ -41,13 +47,13 @@ public class MemberController {
         private final String interest2;
         private final String profileImage;
     }
-    
+
     @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin() {
         return "usr/member/login";
     }
-  
+
     @PreAuthorize("isAnonymous()")
     @GetMapping("/join")
     public String showJoin() {
@@ -61,5 +67,26 @@ public class MemberController {
             return "redirect:/usr/member/join";
         }
         return "redirect:/usr/member/login";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/myPage")
+    public String showMyPage(Model model) {
+        Optional<Member> member = memberService.findByUserId(rq.getMember().getUserId());
+
+        if (member.isEmpty()) {
+            return "redirect:/usr/member/login";
+        }
+
+        Member actor = member.get();
+
+        model.addAttribute("username", actor.getUsername());
+        model.addAttribute("nickname", actor.getNickname().isEmpty() ? "닉네임을 생성해주세요." : actor.getNickname());
+        model.addAttribute("email", actor.getEmail());
+        model.addAttribute("interest1", actor.getInterest1().isEmpty() ? "관심사를 지정해주세요." : actor.getInterest1());
+        model.addAttribute("interest2", actor.getInterest2().isEmpty() ? "관심사를 지정해주세요." : actor.getInterest2());
+        model.addAttribute("emailVerified", actor.getEmailVerified());
+
+        return "/usr/member/myPage";
     }
 }
